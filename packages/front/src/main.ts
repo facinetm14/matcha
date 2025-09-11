f4dc7904a292919aeb65a6035e1c9cd7db904360
Eden FAUCONNIER <nfauconn@f4r1s7.clusters.42paris.fr>
2025-09-11 15:30:45 +0200

    style: rebuilt front with vuetify

--- packages/front/src/main.ts
+++ packages/front/src/main.ts
@@ -1,12 +1,23 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Components
 import App from './App.vue'
-import router from './router'
+
+// Styles
+import 'unfonts.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+registerPlugins(app)
 
 app.mount('#app')
