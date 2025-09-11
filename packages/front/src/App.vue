a69acfafcde98ff99f2d2e415d4d8d97426596c6
Eden FAUCONNIER <nfauconn@f4r1s7.clusters.42paris.fr>
2025-09-11 17:56:41 +0200

    feat: register page
    For now, just a form without any script or anything. Just to push what I did today and try to merge it.

--- packages/front/src/App.vue
+++ packages/front/src/App.vue
@@ -7,3 +7,12 @@
 <script lang="ts" setup>
   //
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
